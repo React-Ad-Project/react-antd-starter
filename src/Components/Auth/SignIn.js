dc6b3b8b002a398741f784b6fa2c419399c9ddab
Ajal333 <ajapoos1999@gmail.com>
2020-04-19 10:50:02 +0530

    Certain changes

--- src/Components/Auth/SignIn.js
+++ src/Components/Auth/SignIn.js
@@ -11,66 +11,77 @@ const SignIn = () => {
   };
 
   return (
-    <div className="frm-algn bg">
-      <Form
-        wrapperCol={{ span: 6 }}
-        name="normal_login"
-        className="login-form"
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Username!",
-            },
-          ]}
-        >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Password!",
-            },
-          ]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-          <Link className="login-form-forgot" to="">
-            Forgot password
-          </Link>
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
+    <div className="bg">
+      <div className="container">
+        <div className="frm-algn">
+          <Form
+            wrapperCol={{ span: 6 }}
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
           >
-            Log in
-          </Button>
-          <br />
-          Or <Link to="">register now!</Link>
-        </Form.Item>
-      </Form>
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Password!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <div className="chkbox">
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+              </div>
+              <div className="frm-link">
+                <Link className="login-form-forgot" to="">
+                  Forgot password
+                </Link>
+              </div>
+            </Form.Item>
+            <Form.Item>
+              <div className="lower-log">
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  Log in
+                </Button>
+              </div>
+              <br />
+              <div className="lower-link">
+                Or <Link to="">register now!</Link>
+              </div>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 };
