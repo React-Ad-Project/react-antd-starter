e461cc26c0841508ec73c8e04cd5762af0af7e14
Ajal333 <ajapoos1999@gmail.com>
2020-04-19 17:48:06 +0530

    Added offer listing card

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { SignUp } from "./Components/Auth/SignUp";
 import SignIn from "./Components/Auth/SignIn";
 import { ProfileEdit } from "./Components/Auth/ProfileEdit";
 import Profile from "./Components/Auth/Profile";
-import OfferViewCard from "./Components/Auth/components/OfferViewCard";
 
 class App extends Component {
   constructor(props) {
@@ -23,7 +22,6 @@ class App extends Component {
           <Route path="/signin" component={SignIn} />
           <Route path="/profile/edit" component={ProfileEdit} />
           <Route path="/profile" component={Profile} />
-          <Route path="/test" component={OfferViewCard} />
           <Route path="/" component={Home} />
         </Switch>
       </BrowserRouter>
