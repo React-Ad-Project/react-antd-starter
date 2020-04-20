6d5df748efe3c6328fd5238720e2ed889d48fc5c
AJ <adhilmp3@gmail.com>
2020-04-20 06:34:12 +0530

    map

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -8,6 +8,44 @@ import { SearchOutlined, FilterOutlined } from "@ant-design/icons";
 import "./index.css";
 
 export default class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      catagories: [
+        {
+          name: "Cafes",
+          img:
+            "https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg",
+        },
+        {
+          name: "Theaters",
+          img:
+            "https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg",
+        },
+        {
+          name: "Electronics",
+          img:
+            "https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg",
+        },
+        {
+          name: "Groceries",
+          img:
+            "https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg",
+        },
+        {
+          name: "Cafes",
+          img:
+            "https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg",
+        },
+        {
+          name: "Cafes",
+          img:
+            "https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="homeMainDiv">
@@ -66,15 +104,9 @@ export default class index extends Component {
         </div>
         <div className="wrapperParent">
           <div className="wrapper">
-            <CategoryCard
-              cardName="Cafes"
-              img="https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg"
-            />
-            <CategoryCard cardName="Theaters" img="https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg"/>
-            <CategoryCard cardName="Electronics"img="https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg" />
-            <CategoryCard cardName="Groceries" img="https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg"/>
-            <CategoryCard cardName="Cafes" img="https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg" />
-            <CategoryCard cardName="Cafes" img="https://img.etimg.com/thumb/width-640,height-480,imgsize-322599,resizemode-1,msid-69997940/indian-caf-chain-market-will-reach-rs-4540-cr-by-2023-report.jpg" />
+            {this.state.catagories.map((item) => {
+              return <CategoryCard cardName={item.name} img={item.img} />;
+            })}
           </div>
         </div>
         <div
